b142e0acbff44e644d915b624ff47d3b4f0777f7
Sebastian-Henao <sebastian.henao03@uceva.edu.co>
2026-09-11 11:09:44 -0500

    feature(atomos): Se crea carpeta y componentes para atomo progress

---
+++ projects/design-system-bootstrap/src/lib/presentation/atoms/progress/progress.atom.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-progress.atom',
+  imports: [],
+  templateUrl: './progress.atom.html',
+  styleUrl: './progress.atom.css',
+})
+export class ProgressAtom {
+
+}
